2fb50d848ef7157ec92f2686efb571c620bc9dd9
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-03-27 22:13:13 +0700

    Update Kuncijawaban1.js

--- Kuncijawaban1.js
+++ Kuncijawaban1.js
@@ -1,4 +1,4 @@
-var points = [40, 100, 1, 5, 25, 10];
-var x = points.sort(function(x, z){return x - z});
-
-console.log(x)
+function evenNumbers(array, number) {
+  let evenNumber = array.filter(num => num % 2 == 0).slice(-number)
+  return evenNumber   
+}
