0da2bf4609dd802b2fa74d7da075f20302ae5e7f
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-04-21 15:44:52 +0700

    Create updateLight.js
    @litanur

---
+++ updateLight.js
@@ -0,0 +1,11 @@
+function updateLight(current) {
+  switch (current){
+    case "green":
+    return "yellow";
+    break;
+  case "yellow":
+    return "red";
+    break;
+  case "red":
+    return "green"};
+}
