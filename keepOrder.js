29246c018aba8f72d2d1bf6485ac849844c2655d
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-04-24 23:00:45 +0700

    Update keepOrder.js
    @litanur

--- keepOrder.js
+++ keepOrder.js
@@ -1,5 +1,6 @@
 function keepOrder(ary, val) {
-  let newArray = ary.concat(val).sort()
+  let newArray = ary.concat(val).sort(function(a, b){return a - b})
   let indexNewArray = newArray.indexOf(val)
   return(indexNewArray)
+
 }
