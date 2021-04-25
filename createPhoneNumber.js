b0baf681584ebfdc3449e44a72f35d213cd32a04
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-04-25 14:49:57 +0700

    Update createPhoneNumber.js

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -9,6 +9,26 @@ let kurungtiga = []
       { kurungdua.push(n[i])}
     for(let i = 6; i < n.length ; i++)
       { kurungtiga.push(n[i])}
+  let kurungsatujadi = kurung.join("")
+  let kurungduajadi = kurungdua.join("")
+  let kurungtigajadi = kurungtiga.join("")
+  return `(${kurungsatujadi}) ${kurungduajadi}-${kurungtigajadi}`
+}
+
+
+//===================== atau yang lebih rumit=================//
+
+function createPhoneNumber(n){
+
+let kurung = []
+let kurungdua = []
+let kurungtiga = []
+  for( let i = 0; i < 3; i++)
+  { kurung.push(n[i])}
+    for(let i = 3; i < 6; i++)
+      { kurungdua.push(n[i])}
+    for(let i = 6; i < n.length ; i++)
+      { kurungtiga.push(n[i])}
   let kurungduatiga = kurungdua.concat("-",kurungtiga).join("")
   let kurungjadi = kurung.join("")
   return `(${kurungjadi}) ${kurungduatiga}`
