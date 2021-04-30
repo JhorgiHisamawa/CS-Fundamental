71cf944d69ab8b72c6a4d3e0b114d9a3a770fdb5
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-04-30 15:22:12 +0700

    Create  getCount.js
    @litanur

---
+++ getCount.js
@@ -0,0 +1,13 @@
+function getCount(str) {
+  //dijadikan array
+  let strA = str.toLowerCase().replace(" ","").split("")
+  let strB = []
+  //dicari index yang mempunyai vowel
+  for(let i = 0; i < strA.length; i++)
+  if(strA[i].match(/[aiueo]/g)) strB.push(strA[i])
+  //dijumlahkan
+  let StrC = 0
+  for(let j = 0; j < strB.length;j++)
+    StrC ++
+   return StrC;
+}
